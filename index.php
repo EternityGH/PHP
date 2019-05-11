ced7129b97375fa0eacd30b83663ae31d96e3f7a
EternityGH <jiawei.4127@gmail.com>
2019-05-11 15:54:52 +0700

    add index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo 1;
+echo 1;
+
+/**
+ * this is comments
+ * this is ...
+ */
+// this is a comment
+
+
+// $a = 12.6;
+// echo $a; // print
+// echo gettype($a);
+// $b = 'Hello';
+// $c = true; // boolean
+// $d = [] ; // array
+
+// class Person{
+// }
+// $e = new Person;
+// echo gettype($e);
+
+
+?>
