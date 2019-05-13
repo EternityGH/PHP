87c4a201b81334db3e094bc64546e832bb770019
EternityGH <jiawei.4127@gmail.com>
2019-05-13 12:18:51 +0700

    Add index

--- form/index.php
+++ form/index.php
@@ -6,35 +6,42 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Form</title>
 </head>
+<?php
+    print_r($_GET);
+?>
 <body>
-    <form id="form-contact" action="">
+    <form metohd="GET">
         <div>
-            Name: <input type="text" placeholder="Enter your name">
+            Name: <input type="text" placeholder="Enter your name" name="txtName">
         </div>
         <div>
             Password: <input type="password" placeholder="Enter your password">
         </div>
         <div>
             Select:
-            <select>
-               
-                <option>HTML</option>
-                <option>CSS</option>
-                <option>PHP</option>
-                <option>Laravel</option>
+            <select name="subject">
+                <option value="html">HTML</option>
+                <option value="css">CSS</option>
+                <option value="php">PHP</option>
+                <option value="laravel">Laravel</option>
             </select>
         </div>
         <div>
-                <input type="radio" name="gender" id="Male"> Male<br>
-                    <label for="female">Male</label>
-                <input type="radio" name="gender" id="Femail"> Female<br>
-                    <label for="female">Female</label>
-                <input type="radio" name="gender" id="Other"> Other
-                    <label for="female">Other</label>
+                <input type="radio" name="gender" id="Male">
+                    <label for="Male">Male</label><br>
+                <input type="radio" name="gender" id="Female">
+                    <label for="Female">Female</label><br>
+                <input type="radio" name="gender" id="Other">
+                    <label for="Other">Other</label>
         </div>
         <div>
             <textarea col="30" rows="5"></textarea>
         </div>
+        <div>
+            Choose file: <input type="file">
+        </div>
+        <button type="submit">Send</button>
     </form>
 </body>
 </html>
+Bài tập: input 1 số a, input 1 số b, dùng select phép tính, hiển thị ra input KQ
